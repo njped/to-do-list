9b9b229fc4ee3d2cc4b80cb6343cd2c851809241
Nathan Pedigo <njpedigo@gmail.com>
2023-02-17 10:35:56 -0700

    working on addTask function to print new taskContainer in the html file

--- app.js
+++ app.js
@@ -1,26 +1,51 @@
 // Global Variables
 const addTask = document.querySelector('.addTask');
+const editTask = document.querySelector('editTask')
 const deleteList = document.querySelector('.deleteList');
 const deleteTask = document.querySelector('.deleteTask');
 const createListBtn = document.querySelector('.createListBtn');
 const lookAtList = document.querySelector('.lists');
 
+let tasksArray = [];
+let listTitleArray = [];
+
 // Global AddEventListener
-addTask.addEventListener('click', addTaskFunction());
-deleteList.addEventListener('click', deleteListFunction());
-deleteTask.addEventListener('click', deleteTaskFunction());
-createListBtn.addEventListener('click', createList());
-lookAtList.addEventListener('click', showClickedList());
+addTask.addEventListener('click', addTaskFunction);
+editTask.addEventListener('click', editCurrentTask)
+deleteList.addEventListener('click', deleteListFunction);
+deleteTask.addEventListener('click', deleteTaskFunction);
+createListBtn.addEventListener('click', createList);
+lookAtList.addEventListener('click', showClickedList);
 
 function addTaskFunction() {
     // plus sign is add empty task under the previous task
-        // make a string that copies the parent taskContainer class and it's children's classes and elements 
+    
+    // make a string that copies the parent taskContainer class and it's children's classes and elements 
+    let newInnerHTML = `
+    <div class="taskContainer">
+        <div class="leftTask">
+            <input type="checkbox" name="Tasks" class="checkbox">
+            <input type="text" name="Tasks" class="taskList" placeholder="Type Tasks Here">
+        </div>
+        <div class="rightTask">
+            <button class="editTask">Edit</button>
+            <i class="fa fa-trash-o deleteTask" style="font-size:24px;"></i>
+        </div>
+    </div>
+    `
+
+    
         //append it after the latest task
 
 }
 
+function editCurrentTask() {
+    // on click grab the current task's input 
+    // when leave input text box save the edit
+}
+
 function deleteListFunction() {
-    // trash can by plus sign is to delete the list
+    // trash can by plus sign is to delete the currentList
         // promt/alert the user if the user wants to delete the current list
         // have two buttons that say Yes or No
 
