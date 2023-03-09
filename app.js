5940402968c56178885379a5ed3e3e26e89ea901
Nathan Pedigo <njpedigo@gmail.com>
2023-03-09 07:29:44 -0700

    trying to figure out why the buttons only work a single time

--- app.js
+++ app.js
@@ -6,25 +6,26 @@ const deleteTask = document.querySelector('.deleteTask');
 const createListBtn = document.querySelector('.createListBtn');
 
 const tasksArray = [];
-const listTitleArray = [];
+const listArray = [];
 
 // Global AddEventListener
-addTask.addEventListener('click', addTaskFunction);
-editTask.addEventListener('click', editCurrentTask)
-deleteList.addEventListener('click', deleteListFunction);
-deleteTask.addEventListener('click', deleteTaskFunction);
 createListBtn.addEventListener('click', createList);
+deleteList.addEventListener('click', deleteListFunction);
+// addTask.addEventListener('click', addTaskFunction);
+// editTask.addEventListener('click', editCurrentTask)
+// deleteTask.addEventListener('click', deleteTaskFunction);
 
 function addTaskFunction() {
     // plus sign is add empty task under the previous task
-    
+    console.log('hello')
     // make a string that copies the parent taskContainer class and it's children's classes and elements
-    let node = document.querySelector('.taskContainer')
+    let node = document.querySelector('.addTaskContainer')
+    let task = document.querySelector('.taskList').value
     let newInnerHTML = `
     <div class="taskContainer">
         <div class="leftTask">
             <input type="checkbox" name="Tasks" class="checkbox">
-            <input type="text" name="Tasks" class="taskList" placeholder="Type Tasks Here">
+            <span>${task}</span>
         </div>
         <div class="rightTask">
             <button class="editTask">Edit</button>
@@ -34,7 +35,7 @@ function addTaskFunction() {
     `;
     // show and append it after the latest task
     node.outerHTML += newInnerHTML;
-    
+
 }
 
 function editCurrentTask() {
@@ -44,26 +45,49 @@ function editCurrentTask() {
 
 function deleteListFunction() {
     // trash can by plus sign is to delete the currentList
-        // promt/alert the user if the user wants to delete the current list
-        // have two buttons that say Yes or No
+    // promt/alert the user if the user wants to delete the current list
+    // have two buttons that say Yes or No
 
 }
 
-function deleteTaskFunction () {
+function deleteTaskFunction() {
     // trash inside task is to delete the current task
-        // when deleting task check which index it is inside the array of tasks 
-        // tasks will be put inside an array
+    // when deleting task check which index it is inside the array of tasks 
+    // tasks will be put inside an array
 
 }
 
-function createList()
-{
+function createList() {
     console.log('hello')
+    let count = 0;
+    let renderTask = document.querySelector('.col-9');
     let listName = document.querySelector('.lists').value;
     let addListName = document.querySelector('.addList');
-    let title = document.getElementById('title').value;
-    title = listName;
+    listArray[count] = listName;
+    count++;
+    let newInnerHTML =
+    `
+        <header class="header">
+            <div class="leftHeader">
+                <h2>${listName}</h2>
+            </div>
+        </header>
+        <div class="greyline"></div>
+        <!--Input Checkboxes go here=-->
+        <main class="listContainer">
+            <div class="addTaskContainer">
+                <div class="leftTask">
+                    <input type="text" name="Tasks" class="taskList" placeholder="Type Tasks Here">
+                </div>
+                <div class="rightTask">
+                    <button class="addTask">Add Task</button>
+                </div>
+            </div>
+            <button class="deleteCheckedTasks">Delete Completed Tasks</button>
+        </main>
+    `
     addListName.outerHTML += `<div class ="listTitle">${listName}</div>`
+    renderTask.innerHTML += newInnerHTML;
 }
 
 function checkCheckBox() {
@@ -73,5 +97,5 @@ function checkCheckBox() {
 
 function showCurrentList() {
     // when clicking on the title name in the side bar print out that lists data
-        // current list in side bar will have its background color changed to darker gray
+    // current list in side bar will have its background color changed to darker gray
 }
